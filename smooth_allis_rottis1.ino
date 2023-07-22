dfc19a2625114ed2c63e2bf3b4d55a9fc4ad942e
lekaawael2 <133268066+lekaawael2@users.noreply.github.com>
2023-07-22 19:47:25 +0300

    Add files via upload

---
+++ smooth_allis_rottis1.ino
@@ -0,0 +1,102 @@
+// C++ code
+//
+int led1=9;
+int led2=10;
+int led3=11;
+int led4=12;
+int led5=13;
+
+void setup()
+{
+pinMode(led1, OUTPUT);
+pinMode(led2, OUTPUT);
+pinMode(led3, OUTPUT);
+pinMode(led4, OUTPUT);
+pinMode(led5, OUTPUT);
+
+}
+
+void loop()
+{
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(12, HIGH); 
+digitalWrite(13,LOW ); 
+delay(1000); 
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(13, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(11, HIGH); 
+delay(1000); 
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(12, HIGH);
+digitalWrite(13, LOW);
+delay(1000); 
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(10, HIGH);
+delay(1000); 
+digitalWrite(10, HIGH);
+digitalWrite(11, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(13, LOW);
+delay(1000);
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(12, HIGH); 
+digitalWrite(13,LOW ); 
+delay(1000); 
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(13, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(11, HIGH); 
+delay(1000); 
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(12, HIGH);
+digitalWrite(13, LOW);
+
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(10, HIGH);
+delay(1000); 
+digitalWrite(10, LOW);
+digitalWrite(11, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(13, LOW);
+delay(1000); 
+digitalWrite(9, HIGH);
+digitalWrite(10, LOW); 
+digitalWrite(11, LOW); 
+digitalWrite(12, LOW);
+digitalWrite(13, LOW);
+delay(1000); 
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(12, HIGH); 
+digitalWrite(13,LOW ); 
+delay(1000); 
+digitalWrite(13, HIGH); 
+delay(1000); 
+digitalWrite(13, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(11, HIGH); 
+delay(1000); 
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(12, HIGH);
+digitalWrite(13, LOW);
+delay(1000); 
+digitalWrite(13, HIGH);
+delay(1000); 
+digitalWrite(10, HIGH);
+delay(1000); 
+digitalWrite(10, HIGH);
+digitalWrite(11, LOW); 
+digitalWrite(12, LOW); 
+digitalWrite(13, LOW);
+
+}
